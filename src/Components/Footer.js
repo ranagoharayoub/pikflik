dae7534b9ef3f9dafdd6fc94f5398a446c6bc136
ShahzaibChaudhry <shahzaibakhter20@gmail.com>
2021-05-27 18:13:42 +0500

    footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,48 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer-cont'>
-            Footer
+            <div className='footer-width'>
+                <div className='footer-left'>
+                    <div className='footer-about'>
+                        <div className='about-heading'>ABOUT</div>
+                        <div className='about-sections'>Frequent Questions<br></br></div>
+                        <div className='about-sections'>Careers<br></br></div>
+                        <div className='about-sections'>Accessibility Statement</div>
+                    </div>
+                    <div className='footer-follow-us'>
+                        <div className='follow-us-heading'>Follow Us</div>
+                        <div className='follow-us-sections'>Instagram<br></br></div>
+                        <div className='follow-us-sections'>Facebook<br></br></div>
+                        <div className='follow-us-sections'>Twitter</div>
+                    </div>
+                </div>
+                <div className='footer-right'>
+                    <div className='footer-help-box'>
+                        <div className='box-left'>
+                            <img className='box-photo' src='Untitled-1-05.png' alt='no'></img>
+                        </div>
+                        <div className='box-right'>
+                            <p className='box-txt-heading'>Need Some Help?</p>
+                            <p className='box-txt'>Talk to someone from our real support team, live 24/7</p>
+                            <button className='footer-btn'>Chat with us</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-lower'>
+                <div className='copyright'>&copy; Copyright 2015-2021 Pikflik, Ltd</div>
+                <div className='footer-links'>
+                    <a className='link' href='www.google.com'>Privacy Policy</a>
+                    <a className='link' href='www.google.com'>Impressum</a>
+                    <a className='link' href='www.google.com'>Terms of Use</a>
+                </div>
+                <div className='language'>
+                    <select className='lan' value='lang'>
+                        <option>English</option>
+                        <option>German</option>
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
