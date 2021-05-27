8a50d17751e02b07c95919ac0d3632dfd567438b
ranagoharayoub <ranagohar1163@gmail.com>
2021-05-27 14:11:40 +0500

    Navbar Completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,14 @@ import './Navbar.css'
 function Navbar() {
     return (
         <div className='navbar-cont'>
-            Navbar
+            <div className='logo'>
+                <img src='/Logo-01.png' width='100px' alt='logo'></img>
+            </div>
+            <div className='nav-btn'>
+                <div className='faq-text'>FAQ</div>
+                <div className='gift-text'>Gift Card</div>
+                <button className='get-btn' >Get Started</button>
+            </div>
         </div>
     )
 }
