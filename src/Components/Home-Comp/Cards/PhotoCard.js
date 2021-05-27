6e2820a8e020c11f789438abf224a96906f899b9
ranagoharayoub <ranagohar1163@gmail.com>
2021-05-27 10:18:37 +0500

    Carousel Completed

--- src/Components/Home-Comp/Cards/PhotoCard.js
+++ src/Components/Home-Comp/Cards/PhotoCard.js
@@ -4,7 +4,9 @@ import './PhotoCard.css'
 function PhotoCard() {
     return (
         <div className='photo-card'>
-            
+            <div><img src='/logo512.png' alt='lpgp'></img></div>
+            <div style={{fontSize:'large', fontWeight:'bold'}}>I'm in love with magic photo! <br></br> Coolest gift ever</div>
+            <div>@raising.a.girl.gang</div>
         </div>
     )
 }
--- src/Components/Home-Comp/HomeMiddle.js
+++ src/Components/Home-Comp/HomeMiddle.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import OwlCarousel from 'react-owl-carousel2';
+import 'react-owl-carousel2/lib/styles.css';
+import 'react-owl-carousel2/src/owl.theme.default.css';
 import PhotoCard from './Cards/PhotoCard'
 import './HomeMiddle.css'
 function HomeMiddle() {
+    const options = {
+        // autoWidth:true,
+        autoheight: true, 
+        items: 4,
+        loop:true,
+        rewind: true,
+        autoplay: true,
+        margin: 10,
+        center: true,
+
+    };
     return (
         <div className='home-middle'>
             <div className='title-sect'>
                 <div className='story-text'>A Story in Every Photo</div>
                 <div className='love-text'>We love seeing the smiles on our customer faces!</div>
             </div>
-            <div className='cards-sect'>
-                <PhotoCard></PhotoCard>
-                <PhotoCard></PhotoCard>
-                <PhotoCard></PhotoCard>
-                
+            <div className='cards-sect' >
+                <OwlCarousel options={options} >
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                    <div className='item'><PhotoCard></PhotoCard></div>
+                </OwlCarousel>
             </div>
         </div>
     )
